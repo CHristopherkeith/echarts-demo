0feee438458ed7b4821449e2881c873537fe59b2
Christopher <easonmu@hotmail.com>
2017-03-30 00:25:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -19,74 +19,111 @@ angular.module('questionnaireApp', ['ui.router'])
         // console.log(data)
 
         var option = {
-                tooltip : {
-                    trigger: 'axis'
-                },
-                toolbox: {
-                    show : true,
-                    feature : {
-                        mark : {show: true},
-                        dataView : {show: true, readOnly: false},
-                        restore : {show: true},
-                        saveAsImage : {show: true}
+            tooltip : {
+                trigger: 'item'
+            },
+            legend: {
+                data:['蒸发量','降水量','最低气温','最高气温']
+            },
+            // toolbox: {
+            //     show : true,
+            //     feature : {
+            //         mark : {show: true},
+            //         dataView : {show: true},
+            //         magicType : {show: true, type: ['line', 'bar']},
+            //         restore : {show: true},
+            //         saveAsImage : {show: true}
+            //     }
+            // },
+            xAxis : [
+                {
+                    type : 'category',
+                    data : [
+                        '1','2','3','4','5','6','7','8','9','10','11','12'
+                    ]
+                }/*,*/
+                // {
+                //     type : 'category',
+                //     data : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+                // }
+            ],
+            yAxis : [
+                {
+                    type : 'value',
+                    axisLine : {    // 轴线
+                        show: true,
+                        lineStyle: {
+                            color: 'red',
+                            type: 'dashed',
+                            width: 2
+                        }
                     }
                 },
-                calculable : true,
-                xAxis : [
-                    {
-                        type : 'category',
-                        // data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
-                        data:xAxisData
-                    }
-                ],
-                yAxis : [
-                    {
-                        type : 'value'
-                    }
-                ],
-                dataZoom : {
-                    show : true,
-                    realtime : true,
-                    //orient: 'vertical',   // 'horizontal'
-                    //x: 0,
-                    y: 36,
-                    // width: 400,
-                    height: 20,
-                    //backgroundColor: 'rgba(221,160,221,0.5)',
-                    //dataBackgroundColor: 'rgba(138,43,226,0.5)',
-                    //fillerColor: 'rgba(38,143,26,0.6)',
-                    //handleColor: 'rgba(128,43,16,0.8)',
-                    // xAxisIndex:[1],
-                    //yAxisIndex:[],
-                    start : 0,
-                    end : 100
-                },
+                {
+                    type : 'value',
+                }
+            ],
+             dataZoom : {
+                show : true,
+                realtime : true,
+                y: 36,
+                height: 20,
+                start : 0,
+                end : 100
+            },
+            series : [
+                {
+                    name: '蒸发量',
+                    type: 'bar',
+                    stack:' ',
+                    data:[2.0, 4.9, 7.0, 23.2, 25.6, 16.7, 0, 0, 32.6, 20.0, 6.4, 3.3]/*,*/
+                    // barWidth:function(option){
 
-                series : [
-                    {
-                        name:'数量',
-                        type:'bar',
-                        itemStyle:{
-                           normal:{
-                            color:"steelblue"
-                           } 
-                        },
-                        // data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
-                        data:data
-                        // markPoint : {
-                        //     data : [
-                        //         {type : 'max', name: '最大值'},
-                        //         {type : 'min', name: '最小值'}
-                        //     ]
-                        // }/*,*/
-                        // markLine : {
-                        //     data : [
-                        //         {type : 'average', name: '平均值'}
-                        //     ]
-                        // }
+                    //     console.log(option)
+
+                    //     return 2;
+
+                        
+
+                    // }()
+                },
+                {
+                    name: '降水量',
+                    type: 'bar',
+                    stack:' ',
+                    // yAxisIndex: 1,
+                    data: [2.6, 5.9, 9.0, 26.4, 28.7, 20.7, 0, 0, 48.7, 18.8, 6.0, 2.3],
+                    itemStyle: {
+                        normal: {
+                            color: function(params) {
+                                var colorList = [
+                                  '#C1232B','#B5C334','#FCCE10','#E87C25','#27727B',
+                                   '#FE8463','#9BCA63','#FAD860','#F3A43B','#60C0DD',
+                                   '#D7504B','#C6E579','#F4E001','#F0805A','#26C0C0'
+                                ];
+                                return colorList[params.dataIndex]
+                            }
+                        }
                     }
-                ]
-            };
+                },
+                {
+                    name: 'xxxx',
+                    type: 'bar',
+                    yAxisIndex: 1,
+                    barMaxWidth:10,
+                    data: [0, 0, 0, 0, 0, 0, 11175.6, 11182.2, 0, 0, 0, 0],
+                    barWidth:2,
+                    // barWidth : function(params){
+
+                    //     // return 2;
+
+                    //     console.log(params)
+
+                    // }
+
+                }
+            ]
+        };
                     
 
         require([
